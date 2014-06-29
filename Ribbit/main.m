bd4734c6b16d522b4668d7000878eeccb8f4a9b2
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-06-29 21:12:49 +0530

    Initial Commit

---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Robin Malhotra on 29/06/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
