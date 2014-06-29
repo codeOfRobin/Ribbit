377409d45b8a66442b048da37f19c4b80fbb255e
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-06-29 23:35:44 +0530

    finished the badge!!

---
+++ Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Robin Malhotra on 29/06/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
